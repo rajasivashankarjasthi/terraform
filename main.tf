df7bacc7e4998260bebb25f9cfe0bfaf33a5c0b9
rajasivashankarjasthi <60227119+rajasivashankarjasthi@users.noreply.github.com>
2020-11-19 17:46:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-access_key ="AKIA27XZL7M52NX5K7ZP"
-secret_key ="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"
+access_key =""
+secret_key =""
 }
 
 resource"aws_instance" "my-first-server"{
